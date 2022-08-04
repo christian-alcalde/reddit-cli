e8e428763bab22ad04a9814d4881d68923f612e3
Christian Alcalde <cc.alcalde@yahoo.com>
2022-08-04 14:09:59 -0700

    add comments

--- reddit.mjs
+++ reddit.mjs
@@ -8,17 +8,19 @@ import yargs from 'yargs';
 // process arguments that are passed in
 const {argv} = yargs(process.argv);
 
+// call to reddit api
 const res = await fetch('https://reddit.com/.json');
 const data = await res.json();
+
 const children = data.data.children;
-const randomPost = children[Math.floor(Math.random() * children.length)];
+const randomPost = children[Math.floor(Math.random() * children.length)]; // random post
 const link = `https://reddit.com${randomPost.data.permalink}`
 
-if(argv.print) {
+if(argv.print) { // if --print is passed as argument, log to console
   console.log(`
     title: ${randomPost.data.title}
     link: ${link}
   `)
-} else {
+} else { // else open in browser
   open(link)
 }
